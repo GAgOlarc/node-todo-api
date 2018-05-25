034a85c462851923643523ac31c7fdd94277d7c0
Andras Nett <andras.nett@gmail.com>
2018-05-25 15:53:12 +0200

    Add new MongoDB URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-const localDb = 'mongodb://localhost:27017/TodoApp';
-const cloudDb = 'mongodb://andrew:0!OE%us8H2@i@ds135290.mlab.com:35290/todoapp';
-const dbPort = process.env.NODE_ENV === 'production' ? cloudDb : localDb;
-mongoose.connect(dbPort);
+// const localDb = 'mongodb://localhost:27017/TodoApp';
+const cloudDb = 'mongodb://andrew:password@ds135290.mlab.com:35290/todoapp';
+// const dbPort = process.env.NODE_ENV === 'production' ? cloudDb : localDb;
+mongoose.connect(cloudDb);
 
 module.exports = {mongoose};
