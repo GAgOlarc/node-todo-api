5555f26b8c2635d9434a0b283a708fa0e92b473e
Andras Nett <andras.nett@gmail.com>
2018-05-25 14:39:25 +0200

    New condition for MongoDB URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://andrew:0!OE%us8H2@i@ds135290.mlab.com:35290/todoapp' || 'mongodb://localhost:27017/TodoApp');
+const localDb = 'mongodb://localhost:27017/TodoApp';
+const cloudDb = 'mongodb://andrew:0!OE%us8H2@i@ds135290.mlab.com:35290/todoapp';
+const dbPort = process.env.PORT ? cloudDb : localDb;
+mongoose.connect(dbPort);
 
 module.exports = {mongoose};
