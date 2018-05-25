6dafd83b9e4e871ef87bff88f967bd7d9dad998a
Andras Nett <andras.nett@gmail.com>
2018-05-25 15:24:08 +0200

    set NODE_ENV=production

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const localDb = 'mongodb://localhost:27017/TodoApp';
 const cloudDb = 'mongodb://andrew:0!OE%us8H2@i@ds135290.mlab.com:35290/todoapp';
-const dbPort = process.env.PORT ? cloudDb : localDb;
+const dbPort = process.env.NODE_ENV === 'production' ? cloudDb : localDb;
 mongoose.connect(dbPort);
 
 module.exports = {mongoose};
