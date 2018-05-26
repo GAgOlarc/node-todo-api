4dd8710ddd78c443cd9f6bf9e54b93a5e372b4c6
Andras Nett <andras.nett@gmail.com>
2018-05-26 13:52:57 +0200

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
     const id = req.params.id;
     if (!ObjectID.isValid(id)) {
-        res.status(404).send('Invalid id');
+        return res.status(404).send('Invalid id');
     }
 
     Todo.findById(id).then(todo => {
@@ -50,7 +50,7 @@ app.delete('/todos/:id', (req, res) => {
     const id = req.params.id;
 
     if (!ObjectID.isValid(id)) {
-        res.status(404).send('Invalid id');
+        return res.status(404).send('Invalid id');
     }
 
     Todo.findByIdAndRemove(id).then(todo => {
